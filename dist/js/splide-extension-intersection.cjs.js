36cd6359e27b3edbac3d778d53f2fbc3c07e607b
Naotoshi Fujita <fujita.naotoshi@gmail.com>
2022-04-05 13:54:47 +0900

    Update to v4.

--- dist/js/splide-extension-intersection.cjs.js
+++ dist/js/splide-extension-intersection.cjs.js
@@ -1,6 +1,6 @@
 /*!
  * @splidejs/splide-extension-intersection
- * Version  : 0.1.6
+ * Version  : 0.2.0
  * License  : MIT
  * Copyright: 2022 Naotoshi Fujita
  */
@@ -8,75 +8,181 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-// node_modules/@splidejs/splide/dist/js/splide.esm.js
-function isArray(subject) {
-  return Array.isArray(subject);
+function empty(array) {
+  array.length = 0;
 }
+
+function slice$1(arrayLike, start, end) {
+  return Array.prototype.slice.call(arrayLike, start, end);
+}
+
+function apply$1(func) {
+  return func.bind.apply(func, [null].concat(slice$1(arguments, 1)));
+}
+
+function typeOf$1(type, subject) {
+  return typeof subject === type;
+}
+
+var isArray = Array.isArray;
+apply$1(typeOf$1, "function");
+apply$1(typeOf$1, "string");
+apply$1(typeOf$1, "undefined");
+
 function toArray(value) {
   return isArray(value) ? value : [value];
 }
+
 function forEach(values, iteratee) {
   toArray(values).forEach(iteratee);
 }
-var EVENT_DESTROY = "destroy";
-function EventInterface(Splide22) {
-  const { event } = Splide22;
-  const key = {};
-  let listeners = [];
-  function on(events, callback, priority) {
-    event.on(events, callback, key, priority);
+
+var ownKeys$1 = Object.keys;
+
+function forOwn$1(object, iteratee, right) {
+  if (object) {
+    var keys = ownKeys$1(object);
+    keys = right ? keys.reverse() : keys;
+
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+
+      if (key !== "__proto__") {
+        if (iteratee(object[key], key) === false) {
+          break;
+        }
+      }
+    }
   }
-  function off(events) {
-    event.off(events, key);
-  }
+
+  return object;
+}
+
+function assign(object) {
+  slice$1(arguments, 1).forEach(function (source) {
+    forOwn$1(source, function (value, key) {
+      object[key] = source[key];
+    });
+  });
+  return object;
+}
+
+function EventBinder() {
+  var listeners = [];
+
   function bind(targets, events, callback, options) {
-    forEachEvent(targets, events, (target, event2) => {
-      listeners.push([target, event2, callback, options]);
-      target.addEventListener(event2, callback, options);
+    forEachEvent(targets, events, function (target, event, namespace) {
+      var isEventTarget = ("addEventListener" in target);
+      var remover = isEventTarget ? target.removeEventListener.bind(target, event, callback, options) : target["removeListener"].bind(target, callback);
+      isEventTarget ? target.addEventListener(event, callback, options) : target["addListener"](callback);
+      listeners.push([target, event, namespace, callback, remover]);
     });
   }
+
   function unbind(targets, events, callback) {
-    forEachEvent(targets, events, (target, event2) => {
-      listeners = listeners.filter((listener) => {
-        if (listener[0] === target && listener[1] === event2 && (!callback || listener[2] === callback)) {
-          target.removeEventListener(event2, listener[2], listener[3]);
+    forEachEvent(targets, events, function (target, event, namespace) {
+      listeners = listeners.filter(function (listener) {
+        if (listener[0] === target && listener[1] === event && listener[2] === namespace && (!callback || listener[3] === callback)) {
+          listener[4]();
           return false;
         }
+
         return true;
       });
     });
   }
+
+  function dispatch(target, type, detail) {
+    var e;
+    var bubbles = true;
+
+    if (typeof CustomEvent === "function") {
+      e = new CustomEvent(type, {
+        bubbles: bubbles,
+        detail: detail
+      });
+    } else {
+      e = document.createEvent("CustomEvent");
+      e.initCustomEvent(type, bubbles, false, detail);
+    }
+
+    target.dispatchEvent(e);
+    return e;
+  }
+
   function forEachEvent(targets, events, iteratee) {
-    forEach(targets, (target) => {
-      if (target) {
-        events.split(" ").forEach(iteratee.bind(null, target));
-      }
+    forEach(targets, function (target) {
+      target && forEach(events, function (events2) {
+        events2.split(" ").forEach(function (eventNS) {
+          var fragment = eventNS.split(".");
+          iteratee(target, fragment[0], fragment[1]);
+        });
+      });
     });
   }
+
   function destroy() {
-    listeners = listeners.filter((data) => unbind(data[0], data[1]));
-    event.offBy(key);
+    listeners.forEach(function (data) {
+      data[4]();
+    });
+    empty(listeners);
   }
-  event.on(EVENT_DESTROY, destroy, key);
+
   return {
-    on,
-    off,
-    emit: event.emit,
-    bind,
-    unbind,
-    destroy
+    bind: bind,
+    unbind: unbind,
+    dispatch: dispatch,
+    destroy: destroy
   };
 }
+var EVENT_DESTROY = "destroy";
 
-// node_modules/@splidejs/splide/src/js/utils/type/type.ts
-function isUndefined2(subject) {
-  return typeof subject === "undefined";
+function EventInterface(Splide2) {
+  var bus = Splide2 ? Splide2.event.bus : document.createDocumentFragment();
+  var binder = EventBinder();
+
+  function on(events, callback) {
+    binder.bind(bus, toArray(events).join(" "), function (e) {
+      callback.apply(callback, isArray(e.detail) ? e.detail : []);
+    });
+  }
+
+  function emit(event) {
+    binder.dispatch(bus, event, slice$1(arguments, 1));
+  }
+
+  if (Splide2) {
+    Splide2.event.on(EVENT_DESTROY, binder.destroy);
+  }
+
+  return assign(binder, {
+    bus: bus,
+    on: on,
+    off: apply$1(binder.unbind, bus),
+    emit: emit
+  });
 }
 
-// node_modules/@splidejs/splide/src/js/utils/object/forOwn/forOwn.ts
-function forOwn2(object, iteratee, right) {
+function slice(arrayLike, start, end) {
+  return Array.prototype.slice.call(arrayLike, start, end);
+}
+
+function apply(func) {
+  return func.bind(null, ...slice(arguments, 1));
+}
+
+function typeOf(type, subject) {
+  return typeof subject === type;
+}
+apply(typeOf, "function");
+apply(typeOf, "string");
+const isUndefined = apply(typeOf, "undefined");
+
+const ownKeys = Object.keys;
+
+function forOwn(object, iteratee, right) {
   if (object) {
-    let keys = Object.keys(object);
+    let keys = ownKeys(object);
     keys = right ? keys.reverse() : keys;
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
@@ -90,61 +196,58 @@ function forOwn2(object, iteratee, right) {
   return object;
 }
 
-// src/js/constants/events.ts
-var EVENT_INTERSECTION = "intersection";
-var EVENT_INTERSECTION_IN = "intersection:in";
-var EVENT_INTERSECTION_OUT = "intersection:out";
+const EVENT_INTERSECTION = "intersection";
+const EVENT_INTERSECTION_IN = "intersection:in";
+const EVENT_INTERSECTION_OUT = "intersection:out";
 
-// src/js/extensions/Intersection/Handlers.ts
-function Handlers(Splide3) {
-  const { Components: Components2 } = Splide3;
+function Handlers(Splide2) {
+  const { Components } = Splide2;
   return {
     keyboard: {
       enable() {
-        Components2.Keyboard.disable(false);
+        Components.Keyboard.disable(false);
       },
       disable() {
-        Components2.Keyboard.disable(true);
+        Components.Keyboard.disable(true);
       }
     },
     autoplay: {
       enable() {
-        if (Splide3.options.autoplay) {
-          Components2.Autoplay.play();
+        if (Splide2.options.autoplay) {
+          Components.Autoplay.play();
         }
       },
       disable() {
-        Components2.Autoplay.pause();
+        Components.Autoplay.pause();
       }
     },
     autoScroll: {
       enable() {
-        const AutoScroll = Components2.AutoScroll;
+        const AutoScroll = Components.AutoScroll;
         AutoScroll && AutoScroll.play();
       },
       disable() {
-        const AutoScroll = Components2.AutoScroll;
+        const AutoScroll = Components.AutoScroll;
         AutoScroll && AutoScroll.pause();
       }
     },
     video: {
       enable() {
-        const Video = Components2.Video;
+        const Video = Components.Video;
         Video && Video.play();
       },
       disable() {
-        const Video = Components2.Video;
+        const Video = Components.Video;
         Video && Video.pause();
       }
     }
   };
 }
 
-// src/js/extensions/Intersection/Intersection.ts
-function Intersection(Splide3, Components2, options) {
-  const { emit } = EventInterface(Splide3);
+function Intersection(Splide2, Components2, options) {
+  const { emit } = EventInterface(Splide2);
   const intersectionOptions = options.intersection || {};
-  const handlers = Handlers(Splide3);
+  const handlers = Handlers(Splide2);
   let observer;
   function mount() {
     if (window.IntersectionObserver) {
@@ -153,7 +256,7 @@ function Intersection(Splide3, Components2, options) {
         rootMargin: intersectionOptions.rootMargin,
         threshold: intersectionOptions.threshold
       });
-      observer.observe(Splide3.root);
+      observer.observe(Splide2.root);
     }
   }
   function destroy() {
@@ -169,8 +272,8 @@ function Intersection(Splide3, Components2, options) {
     }
   }
   function handle(options2) {
-    forOwn2(options2, (value, key) => {
-      if (!isUndefined2(value)) {
+    forOwn(options2, (value, key) => {
+      if (!isUndefined(value)) {
         const handler = handlers[key];
         value ? handler.enable() : handler.disable();
       }
@@ -192,11 +295,5 @@ function Intersection(Splide3, Components2, options) {
     destroy
   };
 }
-/*!
- * Splide.js
- * Version  : 3.6.11
- * License  : MIT
- * Copyright: 2022 Naotoshi Fujita
- */
 
 exports.Intersection = Intersection;
--- dist/js/splide-extension-intersection.js
+++ dist/js/splide-extension-intersection.js
@@ -1,6 +1,6 @@
 /*!
  * @splidejs/splide-extension-intersection
- * Version  : 0.1.6
+ * Version  : 0.2.0
  * License  : MIT
  * Copyright: 2022 Naotoshi Fujita
  */
@@ -9,9 +9,26 @@
 })(function () {
   'use strict';
 
-  function isArray(subject) {
-    return Array.isArray(subject);
+  function empty(array) {
+    array.length = 0;
   }
+
+  function slice$1(arrayLike, start, end) {
+    return Array.prototype.slice.call(arrayLike, start, end);
+  }
+
+  function apply$1(func) {
+    return func.bind.apply(func, [null].concat(slice$1(arguments, 1)));
+  }
+
+  function typeOf$1(type, subject) {
+    return typeof subject === type;
+  }
+
+  var isArray = Array.isArray;
+  apply$1(typeOf$1, "function");
+  apply$1(typeOf$1, "string");
+  apply$1(typeOf$1, "undefined");
 
   function toArray(value) {
     return isArray(value) ? value : [value];
@@ -21,33 +38,53 @@
     toArray(values).forEach(iteratee);
   }
 
-  var EVENT_DESTROY = "destroy";
+  var ownKeys$1 = Object.keys;
 
-  function EventInterface(Splide22) {
-    var event = Splide22.event;
-    var key = {};
+  function forOwn$1(object, iteratee, right) {
+    if (object) {
+      var keys = ownKeys$1(object);
+      keys = right ? keys.reverse() : keys;
+
+      for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+
+        if (key !== "__proto__") {
+          if (iteratee(object[key], key) === false) {
+            break;
+          }
+        }
+      }
+    }
+
+    return object;
+  }
+
+  function assign(object) {
+    slice$1(arguments, 1).forEach(function (source) {
+      forOwn$1(source, function (value, key) {
+        object[key] = source[key];
+      });
+    });
+    return object;
+  }
+
+  function EventBinder() {
     var listeners = [];
 
-    function on(events, callback, priority) {
-      event.on(events, callback, key, priority);
-    }
-
-    function off(events) {
-      event.off(events, key);
-    }
-
     function bind(targets, events, callback, options) {
-      forEachEvent(targets, events, function (target, event2) {
-        listeners.push([target, event2, callback, options]);
-        target.addEventListener(event2, callback, options);
+      forEachEvent(targets, events, function (target, event, namespace) {
+        var isEventTarget = ("addEventListener" in target);
+        var remover = isEventTarget ? target.removeEventListener.bind(target, event, callback, options) : target["removeListener"].bind(target, callback);
+        isEventTarget ? target.addEventListener(event, callback, options) : target["addListener"](callback);
+        listeners.push([target, event, namespace, callback, remover]);
       });
     }
 
     function unbind(targets, events, callback) {
-      forEachEvent(targets, events, function (target, event2) {
+      forEachEvent(targets, events, function (target, event, namespace) {
         listeners = listeners.filter(function (listener) {
-          if (listener[0] === target && listener[1] === event2 && (!callback || listener[2] === callback)) {
-            target.removeEventListener(event2, listener[2], listener[3]);
+          if (listener[0] === target && listener[1] === event && listener[2] === namespace && (!callback || listener[3] === callback)) {
+            listener[4]();
             return false;
           }
 
@@ -56,39 +93,98 @@
       });
     }
 
+    function dispatch(target, type, detail) {
+      var e;
+      var bubbles = true;
+
+      if (typeof CustomEvent === "function") {
+        e = new CustomEvent(type, {
+          bubbles: bubbles,
+          detail: detail
+        });
+      } else {
+        e = document.createEvent("CustomEvent");
+        e.initCustomEvent(type, bubbles, false, detail);
+      }
+
+      target.dispatchEvent(e);
+      return e;
+    }
+
     function forEachEvent(targets, events, iteratee) {
       forEach(targets, function (target) {
-        if (target) {
-          events.split(" ").forEach(iteratee.bind(null, target));
-        }
+        target && forEach(events, function (events2) {
+          events2.split(" ").forEach(function (eventNS) {
+            var fragment = eventNS.split(".");
+            iteratee(target, fragment[0], fragment[1]);
+          });
+        });
       });
     }
 
     function destroy() {
-      listeners = listeners.filter(function (data) {
-        return unbind(data[0], data[1]);
+      listeners.forEach(function (data) {
+        data[4]();
       });
-      event.offBy(key);
+      empty(listeners);
     }
 
-    event.on(EVENT_DESTROY, destroy, key);
     return {
-      on: on,
-      off: off,
-      emit: event.emit,
       bind: bind,
       unbind: unbind,
+      dispatch: dispatch,
       destroy: destroy
     };
   }
 
-  function isUndefined2(subject) {
-    return typeof subject === "undefined";
+  var EVENT_DESTROY = "destroy";
+
+  function EventInterface(Splide2) {
+    var bus = Splide2 ? Splide2.event.bus : document.createDocumentFragment();
+    var binder = EventBinder();
+
+    function on(events, callback) {
+      binder.bind(bus, toArray(events).join(" "), function (e) {
+        callback.apply(callback, isArray(e.detail) ? e.detail : []);
+      });
+    }
+
+    function emit(event) {
+      binder.dispatch(bus, event, slice$1(arguments, 1));
+    }
+
+    if (Splide2) {
+      Splide2.event.on(EVENT_DESTROY, binder.destroy);
+    }
+
+    return assign(binder, {
+      bus: bus,
+      on: on,
+      off: apply$1(binder.unbind, bus),
+      emit: emit
+    });
   }
 
-  function forOwn2(object, iteratee, right) {
+  function slice(arrayLike, start, end) {
+    return Array.prototype.slice.call(arrayLike, start, end);
+  }
+
+  function apply(func) {
+    return func.bind.apply(func, [null].concat(slice(arguments, 1)));
+  }
+
+  function typeOf(type, subject) {
+    return typeof subject === type;
+  }
+
+  apply(typeOf, "function");
+  apply(typeOf, "string");
+  var isUndefined = apply(typeOf, "undefined");
+  var ownKeys = Object.keys;
+
+  function forOwn(object, iteratee, right) {
     if (object) {
-      var keys = Object.keys(object);
+      var keys = ownKeys(object);
       keys = right ? keys.reverse() : keys;
 
       for (var i = 0; i < keys.length; i++) {
@@ -109,56 +205,56 @@
   var EVENT_INTERSECTION_IN = "intersection:in";
   var EVENT_INTERSECTION_OUT = "intersection:out";
 
-  function Handlers(Splide3) {
-    var Components2 = Splide3.Components;
+  function Handlers(Splide2) {
+    var Components = Splide2.Components;
     return {
       keyboard: {
         enable: function enable() {
-          Components2.Keyboard.disable(false);
+          Components.Keyboard.disable(false);
         },
         disable: function disable() {
-          Components2.Keyboard.disable(true);
+          Components.Keyboard.disable(true);
         }
       },
       autoplay: {
         enable: function enable() {
-          if (Splide3.options.autoplay) {
-            Components2.Autoplay.play();
+          if (Splide2.options.autoplay) {
+            Components.Autoplay.play();
           }
         },
         disable: function disable() {
-          Components2.Autoplay.pause();
+          Components.Autoplay.pause();
         }
       },
       autoScroll: {
         enable: function enable() {
-          var AutoScroll = Components2.AutoScroll;
+          var AutoScroll = Components.AutoScroll;
           AutoScroll && AutoScroll.play();
         },
         disable: function disable() {
-          var AutoScroll = Components2.AutoScroll;
+          var AutoScroll = Components.AutoScroll;
           AutoScroll && AutoScroll.pause();
         }
       },
       video: {
         enable: function enable() {
-          var Video = Components2.Video;
+          var Video = Components.Video;
           Video && Video.play();
         },
         disable: function disable() {
-          var Video = Components2.Video;
+          var Video = Components.Video;
           Video && Video.pause();
         }
       }
     };
   }
 
-  function Intersection(Splide3, Components2, options) {
-    var _EventInterface = EventInterface(Splide3),
+  function Intersection(Splide2, Components2, options) {
+    var _EventInterface = EventInterface(Splide2),
         emit = _EventInterface.emit;
 
     var intersectionOptions = options.intersection || {};
-    var handlers = Handlers(Splide3);
+    var handlers = Handlers(Splide2);
     var observer;
 
     function mount() {
@@ -168,7 +264,7 @@
           rootMargin: intersectionOptions.rootMargin,
           threshold: intersectionOptions.threshold
         });
-        observer.observe(Splide3.root);
+        observer.observe(Splide2.root);
       }
     }
 
@@ -189,8 +285,8 @@
     }
 
     function handle(options2) {
-      forOwn2(options2, function (value, key) {
-        if (!isUndefined2(value)) {
+      forOwn(options2, function (value, key) {
+        if (!isUndefined(value)) {
           var handler = handlers[key];
           value ? handler.enable() : handler.disable();
         }
@@ -222,12 +318,5 @@
     window.splide.Extensions = window.splide.Extensions || {};
     window.splide.Extensions.Intersection = Intersection;
   }
-  /*!
-   * Splide.js
-   * Version  : 3.6.11
-   * License  : MIT
-   * Copyright: 2022 Naotoshi Fujita
-   */
-
 });
 //# sourceMappingURL=splide-extension-intersection.js.map
